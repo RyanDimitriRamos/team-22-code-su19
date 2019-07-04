d17bd276cd1494c567f84d438281cb5c4c069bd4
circuit1 <ccui@caltech.edu>
2019-07-04 12:01:35 -0700

    added search functionality for Maps

--- src/main/webapp/js/maps-page-loader.js
+++ src/main/webapp/js/maps-page-loader.js
@@ -10,12 +10,118 @@ function createMap(){
       zoom: 15,
       mapTypeControl: false
     });
+
     var infoWindow = new google.maps.InfoWindow();
 
-    // When the user clicks in the map, show a marker with a text box the user can edit.
-    map.addListener('click', (event) => {
-      createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+    // Try setting map center to user's geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are here.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    }
+
+    /* Search box stuff */
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+    var types = document.getElementById('type-selector');
+    var strictBounds = document.getElementById('strict-bounds-selector');
+
+    map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(card);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
+
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+    
+    // Add controls to the map, allowing users to hide/show features.
+    var styleControl = document.getElementById('style-selector-control');
+    map.controls[google.maps.ControlPosition.TOP_RIGHT.push(styleControl);
+
+    // Apply the JSON when the user chooses to hide/show features.
+    document.getElementById('hide-poi').addEventListener('click', function() {
+      map.setOptions({styles: styles['hide']});
     });
+    document.getElementById('show-poi').addEventListener('click', function() {
+        map.setOptions({styles: styles['default']});
+    });
+
+
+  /* Adds marker from user search */
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    createMarkerFromPlaceSearch(place);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindowContent.children['place-icon'].src = place.icon;
+    infowindowContent.children['place-name'].textContent = place.name;
+    infowindowContent.children['place-address'].textContent = address;
+    infowindow.open(map, marker);
+  });
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      autocomplete.setTypes(types);
+    });
+  }
+
+  setupClickListener('changetype-all', []);
+  setupClickListener('changetype-address', ['address']);
+  setupClickListener('changetype-establishment', ['establishment']);
+  setupClickListener('changetype-geocode', ['geocode']);
+
+  document.getElementById('use-strict-bounds')
+      .addEventListener('click', function() {
+        console.log('Checkbox clicked! New state=' + this.checked);
+        autocomplete.setOptions({strictBounds: this.checked});
+      });
+
+    // When the user clicks in the map, show a marker with a text box the user can edit.
+    // map.addListener('click', (event) => {
+    //   createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+    // });
 
     fetchMarkers();
 
@@ -40,47 +146,8 @@ function createMap(){
           addLandmark(map, restaurant.lat, restaurant.lng, restaurant.name);
       });
     });
-    
-    // Add controls to the map, allowing users to hide/show features.
-    var styleControl = document.getElementById('style-selector-control');
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(styleControl);
-
-    // Apply the JSON when the user chooses to hide/show features.
-    document.getElementById('hide-poi').addEventListener('click', function() {
-      map.setOptions({styles: styles['hide']});
-    });
-    document.getElementById('show-poi').addEventListener('click', function() {
-        map.setOptions({styles: styles['default']});
-    });
-
-    // Try HTML5 geolocation.
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-    
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-      });
-            } else {
-              // Browser doesn't support Geolocation
-              handleLocationError(false, infoWindow, map.getCenter());
-            }
-
 }
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
+
 
 
 /** Adds a marker that shows an info window when clicked. */
@@ -117,6 +184,21 @@ function createMarkerForDisplay(lat, lng, content){
   });
 
 }
+
+/** Creates a marker that shows a read-only info window when clicked. */
+function createMarkerFromPlaceSearch(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+
 /** Sends a marker to the backend for saving. */
 function postMarker(lat, lng, content){
   const params = new URLSearchParams();
@@ -147,6 +229,7 @@ function createMarkerForEdit(lat, lng){
   });
   infoWindow.open(map, editMarker);
 }
+
 /** Builds and returns HTML elements that show an editable textbox and a submit button. */
 function buildInfoWindowInput(lat, lng){
   const textBox = document.createElement('textarea');
