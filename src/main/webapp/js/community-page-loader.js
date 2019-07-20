7f69499ffab28358516e97c234652a767065fb31
Jeremy Lema <jyl2172@columbia.edu>
2019-07-19 21:20:29 -0700

    Got user community page to show names, links

--- src/main/webapp/js/community-page-loader.js
+++ src/main/webapp/js/community-page-loader.js
@@ -17,17 +17,23 @@
 
 /** Fetches users and adds them to the page. */
 function fetchUserList(){
-	const url = '/user-list';
-	fetch(url).then((response) => {
-		return response.json();
-	}).then((users) => {
+        const url = '/tables';
+        fetch(url).then((response) => {
+                return response.json();
+        }).then((tables) => {
 		const list = document.getElementById('list');
 		list.innerHTML = '';
 
-		users.forEach((user) => {
-			const userListItem = buildUserListItem(user);
+		tables.forEach((table) => {
+			const userListItem = buildUserListItem(table);
 			list.appendChild(userListItem);
 	  	});
+                // initialized masonry
+                var elem = document.querySelector('#list');
+                var msnry = new Masonry( elem, {
+                   itemSelector: '.list',
+                   percentPosition: true
+                });
 	});
 }
 
@@ -35,10 +41,20 @@ function fetchUserList(){
 * Builds a list element that contains a link to a user page, e.g.
 * <li><a href="/user-page.jsp?user=test@example.com">test@example.com</a></li>
 */
-function buildUserListItem(user){
-	const userLink = document.createElement('a');
-	userLink.setAttribute('href', '/user-page.jsp?user=' + user);
-	userLink.appendChild(document.createTextNode(user));
+function buildUserListItem(table){
+
+        const userLink = document.createElement('a');
+        if(table.email.length == 0){
+          userLink.setAttribute('href', '/user-page.jsp');
+        }
+        else{
+          userLink.setAttribute('href', '/user-page.jsp?user=' + table.email);
+        }
+        var name = table.firstName + " " + table.lastName;
+        if(table.firstName.length == 0){
+           name = "No name was provided";
+        }
+	userLink.appendChild(document.createTextNode(name));
 	const userListItem = document.createElement('li');
 	userListItem.appendChild(userLink);
 	return userListItem;
